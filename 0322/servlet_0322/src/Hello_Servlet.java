6d6d67c260474b98c1274a065d8e32502474c00e
KangSuMin0611 <76684366+KangSuMin0611@users.noreply.github.com>
2021-03-22 11:45:24 +0900

    Add files via upload

---
+++ 0322/servlet_0322/src/Hello_Servlet.java
@@ -0,0 +1,61 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Hello_Servelet
+ */
+@WebServlet("/Hello_Servelet")
+public class Hello_Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Hello_Servlet() {
+        super();
+        
+        
+        
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Hello Sevlet</title>");
+		out.println("<head>");
+		out.println("<body>");
+		out.println("<h2> Hello Sevlet </h2>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Hello Sevlet</title>");
+		out.println("<head>");
+		out.println("<body>");
+		out.println("<h2> Hello Sevlet </h2>");
+		out.println("</body>");
+		out.println("</html>");
+	
+	}
+
+}
